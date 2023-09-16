b7815269eb1edf4b3881c4e9d23198570c500951
Gustavo Duarte <81310847+gustavo-duartee@users.noreply.github.com>
2023-09-16 19:01:17 -0300

    1.3

---
+++ Consultorio_Dentista/Consultorio_Dentista.Model/Services/ServiceConsultas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Consultorio_Dentista.Model.Repository;
+
+namespace Consultorio_Dentista.Model.Services
+{
+    public class ServiceConsultas
+    {
+        public RepositoryConsultas oRepositoryConsultas { get; set; }
+
+        public ServiceConsultas()
+        {
+            oRepositoryConsultas = new RepositoryConsultas();
+        }
+    }
+}
---
+++ Consultorio_Dentista/Consultorio_Dentista/Controllers/ConsultasController.cs
@@ -0,0 +1,23 @@
+﻿using Consultorio_Dentista.Model.Services;
+using Consultorio_Dentista.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Consultorio_Dentista.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private ServiceConsultas _ServicioConsultas;
+
+        public ConsultasController()
+        {
+            _ServicioConsultas = new ServiceConsultas();    
+        }
+        public async Task<IActionResult> Index()
+        {   
+            var ListaConsultas = await _ServicioConsultas.oRepositoryConsultas.SelecionarTodosAsync();
+            return View(ListaConsultas);
+        }
+
+    }
+}
