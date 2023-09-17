5b8abb3442855921d88904bd450bc77a8c9b33fa
Gustavo Duarte <81310847+gustavo-duartee@users.noreply.github.com>
2023-09-16 23:37:29 -0300

    1.4

--- Consultorio_Dentista/Consultorio_Dentista/Controllers/ConsultasController.cs
+++ Consultorio_Dentista/Consultorio_Dentista/Controllers/ConsultasController.cs
@@ -7,16 +7,69 @@ namespace Consultorio_Dentista.Controllers
 {
     public class ConsultasController : Controller
     {
-        private ServiceConsultas _ServicioConsultas;
+        private ServiceConsultas _ServiceConsultas;
 
         public ConsultasController()
         {
-            _ServicioConsultas = new ServiceConsultas();    
+            _ServiceConsultas = new ServiceConsultas();    
         }
         public async Task<IActionResult> Index()
         {   
-            var ListaConsultas = await _ServicioConsultas.oRepositoryConsultas.SelecionarTodosAsync();
+            var ListaConsultas = await _ServiceConsultas.oRepositoryConsultas.SelecionarTodosAsync();
             return View(ListaConsultas);
+        }
+        public void CarregaDadosViewBag()
+        {
+            ViewData["Id"] = new SelectList(_ServiceConsultas.oRepositoryConsultas.SelecionarTodos(), "ID_Cliente","Data_Consulta", "Hora_Consulta", "Descricao");
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            CarregaDadosViewBag();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Consultas consultas)
+        {
+            CarregaDadosViewBag();
+            if(ModelState.IsValid)
+            {
+                consultas = await _ServiceConsultas.oRepositoryConsultas.IncluirAsync(consultas); 
+                return View(consultas);
+            }
+            else
+            {
+                return View(consultas);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var tipoConsulta = await _ServiceConsultas.oRepositoryConsultas.SelecionarPkAsync(id);
+            return View(tipoConsulta);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Consultas consulta)
+        {
+            if(ModelState.IsValid)
+            {
+                var tipoConsulta = await _ServiceConsultas.oRepositoryConsultas.AlterarAsync(consulta);
+                return View(tipoConsulta);
+            }
+            ViewData["MensagemErro"] = "Ocorreu um erro";
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _ServiceConsultas.oRepositoryConsultas.ExcluirAsync(id);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var tipoConsulta = await _ServiceConsultas.oRepositoryConsultas.SelecionarPkAsync(id);
+            return View(tipoConsulta);
         }
 
     }
